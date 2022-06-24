f2a9bab39f5e81d92c4ef14ca8652f3a9d40ee0b
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-06-24 01:07:02 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
-        <p className="text-2xl bg-gray-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-white-900">
+        <p className="text-2xl bg-black-500 font-bold text-white py-1 px-2 dark:bg-white-50 dark:text-black-900">
           HS search 🔎
         </p>
       </Link>
