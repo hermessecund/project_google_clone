f3879b6c771b86af4f754c7db9e800e5706d89f2
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-06-24 02:17:37 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex justify-between items-center space-x-5 w-screen ">
       <Link to="/">
-       <p><span>&#10042;</span><h1 style="color:#00FF99">hermes search</h1>🔎
+       <p><h1 style="color:#00FF99">hermes search</h1>🔎
         </p>
       </Link>
-      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">{darkTheme ? '💡 Light' : '🌙 Night'}</button>
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-20 dark:text-gray-900 bg-white border px-2 py-1 hover:shadow-lg">{darkTheme ? '🌕 Light' : '🌑 Night'}</button>
     </div>
     <Search />
   </div>
