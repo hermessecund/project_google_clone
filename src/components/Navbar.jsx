2bd3f2427eeae5120deef770c8d60dee21350f30
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-06-24 02:48:00 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Search } from './Search';
 
 export const Navbar = ({ setDarkTheme, darkTheme }) => (
   <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 ">
-    <div className="flex justify-between items-center space-x-5 w-screen ">
+    <div className="flex justify-between items-center space-x-10 w-screen ">
       <Link to="/">
         <p className="text-4xl bg-black-500 font-bold text-green py-1 px-2 dark:bg-white-50 dark:text-black-500">
           hermes search
