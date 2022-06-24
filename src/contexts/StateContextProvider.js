28ab39ad29b7cf942eec1d7dd661f1b9edeff155
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-06-24 01:28:55 +0000

    Update StateContextProvider.js

--- src/contexts/StateContextProvider.js
+++ src/contexts/StateContextProvider.js
@@ -14,8 +14,10 @@ export const StateContextProvider = ({ children }) => {
     const res = await fetch(`${baseUrl}${url}`, {
       method: 'GET',
       headers: {
-        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+    'X-User-Agent': 'desktop',
+    'X-Proxy-Location': 'EU',
+    'X-RapidAPI-Key': '314f215117msha3abb1b6c2a8bcdp1bd2bdjsnee6788226a87',
+    'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
       },
     });
 
