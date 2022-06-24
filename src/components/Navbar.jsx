b89b511178e571b97593f39d5710727181a99004
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-06-24 03:52:30 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Search } from './Search';
 
 export const Navbar = ({ setDarkTheme, darkTheme }) => (
-  <div className="p-5 pb-0 flex flex-wrap sm:center-between justify-right items-center border-b dark:border-gray-700 border-gray-200 ">
+  <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-left items-left border-b dark:border-gray-700 border-gray-200 ">
     <div className="flex center-between items-right space-x-2 w-screen ">
       <Link to="/">
         <p className="text-4xl bg-black-500 font-bold text-green py-1 px-2 dark:bg-white-50 dark:text-black-500">
