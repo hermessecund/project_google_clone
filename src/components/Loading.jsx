dcc099487f38c334ea244d88e6ddd56f9bef4ee1
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-10 13:23:28 +0000

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,6 +3,6 @@ import Loader from 'react-loader-spinner';
 
 export const Loading = () => (
   <div className="flex justify-center items-center ">
-    <Loader type="Puff" color="#00BFFF" height={550} width={80} />
+    <Loader type="polygon" color="#00BFFF" height={250} width={30} />
   </div>
 );
